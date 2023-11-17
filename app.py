cff4d5cfd5da8e6484de9277a82795df83648cf1
Tong <yotsapol.tong@gmail.com>
2023-11-17 18:23:52 +0700

    update more API

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
 from flask import Flask, request
+from scapy.all import *
+# from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+# socketio = SocketIO(app, cors_allowed_origins='*')
+
+def sendMessage(pkt):
+    message = str(pkt.payload.src) + " " 
+    + str(pkt.payload.payload.sport) + " " 
+    + str(pkt.payload.dst) + " " 
+    + str(pkt.payload.payload.dport) + " " 
+    + getProtocol(pkt) + " " 
+    + str(len(pkt)) + " " 
+    + "Information"
+
+    send(message)
+
+def getPacketInfo(pkt):
+    pktInfo = ""
+    pktInfo += ("Source IP: " + str(pkt.payload.src) + " ") 
+    pktInfo += ("Source port: " + str(pkt.payload.payload.sport) + " ") 
+    pktInfo += ("Destination IP: " + str(pkt.payload.dst) + " ") 
+    pktInfo += ("Destination port: " + str(pkt.payload.payload.dport) + " ")
+    pktInfo += ("Protocol: " + getProtocol(pkt) + " ")
+    pktInfo += ("Length: " + str(len(pkt)))
+    return pktInfo
+
+def getProtocol(pkt):
+    if(str(pkt.payload.payload.sport) == '53' or (pkt.payload.payload.dport) == '53'):
+        return 'DNS'
+    else:
+        if str(pkt.payload.proto) == '6':
+            return 'TCP'
+        else:
+            return 'UDP'
 
 def allow_file(filename):
-    print('.' in filename)
-    print(filename.rsplit('.', 1)[1].lower() in {
-        'pcap',
-        'cap'
-    })
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {
         'pcap',
         'cap'
     }
+
+def getNetworkInterface():
+    interfaceNames = conf.ifaces
+    return interfaceNames
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/dashboard', methods=['GET', 'POST'])
@@ -23,15 +55,83 @@ def dashboard():
             return {'error': "No file provided"}, 400
         
         imported_file = request.files['file']
-        print(imported_file)
         
         if imported_file == "":
             return {'error': "No file selected"}, 400
         if imported_file and allow_file(imported_file.filename):   
-            return {'text': "imported file is ready!"}
+            packetFromFile = rdpcap(imported_file)
+            packetList = []
+            noPacket = 0
+            for packet in packetFromFile:
+                noPacket += 1
+                packetList.append('No.:' + str(noPacket) + ' ' + getPacketInfo(packet))
+
+            streamDict = {}
+            protocolDict = {}
+            for row in packetList:
+                packetNo = row.split("No.:")[1].split()[0].strip()
+                sourceIP = row.split("Source IP:")[1].split()[0].strip()
+                sourcePort = row.split("Source port:")[1].split()[0].strip()
+                destinationIP = row.split("Destination IP:")[1].split()[0].strip()
+                destinationPort = row.split("Destination port:")[1].split()[0].strip()
+
+
+                if bool(streamDict):
+                    sameStream = 0
+                    for key in streamDict:
+                        if (sourceIP in streamDict[key]['IP']) and (destinationIP in streamDict[key]['IP']) and (sourcePort in streamDict[key]['Port']) and (destinationPort in streamDict[key]['Port']):
+                            oldValue = streamDict[key]['PacketNo.']
+                            streamDict[key]['PacketNo.'] = oldValue + ',' + packetNo
+                            sameStream = 1
+                            break
+                    if sameStream == 0:
+                        newKey = 'stream#' + str(len(streamDict) + 1)
+                        streamDict[newKey] = {
+                            'IP': sourceIP + ', ' + destinationIP,
+                            'Port': sourcePort + ', ' + destinationPort,
+                            'PacketNo.': packetNo
+                        }
+                else:
+                    streamDict['stream#1'] = {
+                        'IP': sourceIP + ', ' + destinationIP,
+                        'Port': sourcePort + ', ' + destinationPort,
+                        'PacketNo.': packetNo
+                    }
+
+                protocol = row.split("Protocol:")[1].split()[0].strip()
+                if protocol in protocolDict:
+                    protocolDict[protocol] += 1
+                else:
+                    protocolDict[protocol] = 1
+
+            packetList.append(protocolDict)
+            packetList.append(streamDict)
+            return packetList
         else:
             return {'error': "Invalid file format, allow file types are .pacp or .cap"}, 400
         
+@app.route('/packet_capture', methods=['GET', 'POST'])
+def packet_capture():
+    if request.method == 'GET':
+        interfaceList = []
+        for key in conf.ifaces.data:
+            interfaceList.append(conf.ifaces.data[key].name)
+        return {'interfaceList': interfaceList}
+    else:
+        # sniffer = AsyncSniffer(iface=request.form['interfaceName'], prn=lambda msg: sendMessage(msg))
+        # if request.form['stop'] == "0":
+        #     sniffer.start()
+        # else:
+        #     result = sniffer.stop()
+        #     wrpcap("result.pcap", result)
 
+        packetList = sniff(iface=request.form['interfaceName'], count=int(request.form['count']), filter='ip', prn=lambda x: x.show())
+        resultList = []
+        for packet in packetList:
+            resultList.append(getPacketInfo(packet))
+        wrpcap("result.pcap", packetList)
+        return resultList
+
+        
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=10000)
--- myPacketSniffer.py
+++ myPacketSniffer.py
@@ -1,23 +1,34 @@
 from scapy.all import *
 
 #get all network interfaces
-# print(type(conf.ifaces)) 
+# print(type(conf.ifaces.data))
+# ifaceList = []
+# for key in conf.ifaces.data:
+#     ifaceList.append(conf.ifaces.data[key].name)
 
-# a = sniff(count=50, iface="Intel(R) Ethernet Connection (14) I219-V")
+# print(ifaceList)
+
+a = sniff(count=5, iface="Ethernet")
+wrpcap("tmp.pcap", a)
+# r = rdpcap('tmp.pcap')
+# r.summary()
+
+# print(str(a[0].show))
+
 # a = sniff(count=20, iface="Ethernet", filter="udp")
 # a.show()
 # sniff(iface="Ethernet", prn=lambda x: x.show(), count=5)
-r = rdpcap('./capture01.pcap')
+# r = rdpcap('./capture01.pcap')
 # print(get_if_addr("Intel(R) Ethernet Connection (14) I219-V"))
 # r = sniff(offline='capture01.pcap')
 # r.plot(lambda x: x.id)
 # r.show()
-packet04_l2 = r[10]
+# packet04_l2 = r[10]
 # packet04_l2_0 = r[0]
 # len(packet04_l2)
 # packet04_l3 = packet04_l2.payload
 # packet04_l4 = packet04_l3.payload
-packet04_l2.show()
+# packet04_l2.show()
 # print(len(packet04_l4.payload))
 # print(type(packet04_l3.payload) is scapy.layers.inet.TCP)
 # print(str(packet04_l2).find("UDP"))
@@ -32,3 +43,27 @@ packet04_l2.show()
 # hexdump(pkt)
 
 # lsc() #show fn.
+
+
+
+# packet = r[3]
+# for packet in r:
+#     print(packet.show())
+
+# def getProtocol(pkt):
+#     if(str(pkt.payload.payload.sport) == '53' or (pkt.payload.payload.dport) == '53'):
+#         return 'DNS'
+#     else:
+#         if str(pkt.payload.proto) == '6':
+#             return 'TCP'
+#         else:
+#             return 'UDP'
+        
+# print(str(packet[0].payload.src) + " " +
+#       str(packet[0].payload.payload.sport) + " " +
+#       str(packet[0].payload.dst) + " " +
+#       str(packet[0].payload.payload.dport) + " " +
+#       getProtocol(packet[0]) + " " +
+#       str(len(packet[0])) + " " +
+#       "Information"
+#       )
